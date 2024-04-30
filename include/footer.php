7b4382c63c30d6917255c38a4e62b6ae91abd542
Shravan K <106543826+shrmax@users.noreply.github.com>
2024-04-30 15:16:29 +0530

    Add files via upload
    minor project front end

---
+++ include/footer.php
@@ -0,0 +1,40 @@
+<footer>
+        <div class="div17">
+            <div>
+                <h2 class="head4">Contact Us</h2>
+            </div>
+            <div>
+                <p class="p6">F-611 Tower 2, Seawoods Grand Central,</p>
+                <p class="p6">Navi Mumbai 400706 INDIA</p>
+                <p class="p6">Email: ‍<a href="mailto:‍‍‍contact@chalo.com" class="p7">
+                    contact@chalo.com</a></p>
+                <p class="p6">Corporate Identification Number (CIN):</p>
+                <p class="p6">U72900MH2014PTC254181</p>
+            </div>
+        </div>
+
+        <div class="div18">
+            <div>
+                <h2 class="head5">Social Connect</h2>
+            </div>
+            <div class="div19">
+                <a href="https://www.facebook.com/chaloapp">
+                    <img src="\Chalo\Images\fb.jpg" class="img3"></img>
+                </a>
+                <a href="https://www.instagram.com/chalo.app/">
+                    <img src="\Chalo\Images\insta.png" class="img3"></img>
+                </a>
+                <a href="https://www.linkedin.com/company/chaloapp">
+                    <img src="\Chalo\Images\linkedIn.jpg" class="img3"></img>
+                </a>
+                <a href="https://twitter.com/chaloapp">
+                    <img src="\Chalo\Images\twit.jpg" class="img3"></img>
+                </a>
+                <a href="https://www.youtube.com/channel/UCKSIJ56JRt7YrG6hFQ-wMYA">
+                    <img src="\Chalo\Images\YouTube.jpg" class="img3"></img>
+                </a>
+            </div>
+        </div>
+    </footer>
+ </body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css\index-style.css">
+    <link rel="stylesheet" href="css\chalo_card.css">
+    <title>Chalo Website</title>
+</head>
+
+<body>
+    <script src="JS\header.js"></script>
+    <header>
+        <nav class="nav1">
+            <div class="div1">
+                <img src="\Chalo\Images\chalologo2.png" height="70px">
+                <ul class="ul1">
+                    <li>
+                        <a href="\Chalo\index.php" class="a1">Chalo App</a>
+                    </li>
+                    <li>
+                        <a href="\Chalo\Home_Add\chalo_card.php" class="a1">Chalo Card</a>
+                    </li>
+                    <li>
+                        <a href="\Chalo\Home_Add\mobile_pass.php" class="a1">Mobile Pass</a>
+                    </li>
+                    <li>
+                        <a href="\Chalo\Home_Add\mobile_ticket.php" class="a1">Mobile Ticket</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="div3">
+                <button class="log">
+                    Login/SignUp
+                </button>
+                <button class="butn">
+                    <a href='https://play.google.com/store/apps/details?id=app.zophop&amp;hl=en" target="_blank"' style="text-decoration:none;color:white;">
+                    Download App</a>
+                </button>
+            </div>
+        </nav>
+        <hr>
+    </header>
